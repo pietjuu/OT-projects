ed20708899de8be28188d9bd9d1de8c645f9d57a
Pieter Verheye <PVER@EASI.NET>
2025-06-03 21:25:15 +0200

    add flask functionality

--- WebApp-OT/webserverThatSendsModbus.py
+++ WebApp-OT/webserverThatSendsModbus.py
@@ -20,6 +20,29 @@ def writecoil(address, value):
 def readcoil(address, value):
     client = ModbusTcpClient(MODBUS_SERVER_IP, port=MODBUS_PORT)
     client.connect()
-    client.read_coils(address)
+    result =client.read_coils(address)
     client.close()
+    if not result.isError():
+        return result.bits[0]
+    return None
 
+# When user visit home page (/) run readcoil function and store it in coil_0, then store that in the html
+@app.route("/")
+def index():
+    coil_0 = readcoil(0)
+    return render_template("index.html", coil_0=coil_0)
+
+# When user clicks the coil/on run write coil function and pass True to it
+@app.route("/coil/on")
+def coil_on():
+    writecoil(0, True)
+    return redirect(url_for("index"))
+
+# When user clicks the coil/ff run write coil function and pass False to it
+@app.route("/coil/off")
+def coil_off():
+    writecoil(0, False)
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(host="O.0.0.0", port=5000, debug=True)
