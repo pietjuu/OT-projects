a78c4298733ba686efe48f934c699393d5cb32ef
Pieter <pieter.verheye@gmail.com>
2026-07-22 22:43:10 +0200

    add test LED with pushbutton

--- OPC UA/testLED.py
+++ OPC UA/testLED.py
@@ -1,11 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
+BUTTON_PIN = 16
 LED_PIN = 26
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.output(LED_PIN, GPIO.HIGH)  # aan
-time.sleep(1)
-GPIO.output(LED_PIN, GPIO.LOW)   # uit
-GPIO.cleanup()
+print("Druk op de knop... (Ctrl+C om te stoppen)")
+
+try:
+    while True:
+        if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+            GPIO.output(LED_PIN, GPIO.HIGH)
+            print("Knop ingedrukt, LED aan")
+        else:
+            GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
